48d97530d0831ec6a969b3278e9b2260f13b1a88
Dannysyed <dannysyed20@gmail.com>
2024-05-18 23:33:38 -0400

    Setup map using mapbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
-import Map from "./Map";
-
+import React, { useEffect, useRef } from "react";
+import Map from "./components/Map";
 const App = () => {
+  let map = useRef(null);
+  useEffect(() => {
+    if (map.current) {
+      // Access the DOM element
+      map.current.style.color = "red";
+    }
+  }, []);
+
   return (
     <div>
-      <h1>Mapbox in React</h1>
+      <h1 ref={map}>Mapbox in React</h1>
       <Map />
     </div>
   );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,3 +1,4 @@
+// src/Map.js
 import React, { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
@@ -5,25 +6,47 @@ import "mapbox-gl/dist/mapbox-gl.css";
 const Map = () => {
   const mapContainer = useRef(null);
   const map = useRef(null);
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiZGFuaWVsc3llZDIwIiwiYSI6ImNsd2NxMjJmYTA1NncyamxlcmhjNnpqZWIifQ.znFfrrla2t7suhHOwCfyuQ";
 
-  // Replace with your Mapbox access token
-  mapboxgl.accessToken = "YOUR_MAPBOX_ACCESS_TOKEN";
+  const pointsOfInterest = [
+    {
+      id: 1,
+      name: "University of Waterloo",
+      coordinates: [-80.53733, 43.468],
+    },
+    { id: 2, name: "Statue of Liberty", coordinates: [-74.0445, 40.6892] },
+    { id: 3, name: "Times Square", coordinates: [-73.9851, 40.758] },
+  ];
 
   useEffect(() => {
     if (map.current) return; // Initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v11", // Style URL
-      center: [-74.5, 40], // Initial center [lng, lat]
+      center: [-80.51667, 43.466667], // Initial center [lng, lat]
       zoom: 9, // Initial zoom level
     });
-  }, []);
 
-  return (
-    <div>
-      <div ref={mapContainer} style={{ width: "100%", height: "400px" }} />
-    </div>
-  );
+    // pointsOfInterest.forEach((poi) => {
+    //   new mapboxgl.Marker() // Initialize and add each marker
+    //     .setLngLat(poi.coordinates)
+    //     .setPopup(new mapboxgl.Popup().setText(poi.name)) // Add popups
+    //     .addTo(map.current);
+    // });
+    pointsOfInterest.forEach((poi) => {
+      const popup = new mapboxgl.Popup().setHTML(`
+          <h3>${poi.name}</h3>
+          <p>Description of ${poi.name}</p>
+          <img src="URL_TO_IMAGE" alt="${poi.name}" style="width:100px;height:auto;"/>
+        `);
+      new mapboxgl.Marker()
+        .setLngLat(poi.coordinates)
+        .setPopup(popup) // Add custom popups
+        .addTo(map.current);
+    });
+  });
+  return <div ref={mapContainer} style={{ width: "100%", height: "500px" }} />;
 };
 
 export default Map;
